d5467245f5d6808a55fe32bc3216c95a0f8512f5
Alexandre BOISSEL <alexandre.boissel@epitech.digital>
2024-09-05 00:00:26 +0200

    partie 3-4

--- Day2/partie2.py
+++ Day2/partie2.py
@@ -23,10 +23,3 @@ def native_csv_write(file: str, headers: list, data: list[tuple]):
         for row in data:
             csv_w.writerow(row[1:])
 
-    
-if __name__ == "__main__":
-    csv_file = "Day2/assets/orders_semicolon.csv"
-    print(native_csv_read(csv_file))
-    
-    native_csv_write('output.csv', ["OrderID", "Product", "Quantity"], [(1, "Banana", 10), (2, "Apple", 5), (3, "Orange", 3)])
-
